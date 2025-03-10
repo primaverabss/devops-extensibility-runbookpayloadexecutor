8e3d85da91daaf2f501d08489e60f91cf193db4b
Alex Silva <alsilva@cegid.com>
2025-03-10 11:21:23 +0000

    add dynamic_install_allow_preview and use_dynamic_install

--- src/task/task.ps1
+++ src/task/task.ps1
@@ -45,8 +45,9 @@ if ([string]::IsNullOrEmpty($changeContext)) {
 }
 Write-Host "[ INFO ] Context updated"
 
+az config set extension.use_dynamic_install=yes_without_prompt
+az config set extension.dynamic_install_allow_preview=true
 az config set core.allow_experimental=true
-
 az config set core.allow_bundled_experimental=true
 
 
